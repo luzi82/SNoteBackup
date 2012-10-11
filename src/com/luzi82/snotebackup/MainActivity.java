9b459f196f948f26aa06687a06eed94434a612c0
tszkinleung <tszkin.leung@fulldynamic.com>
2012-10-11 10:24:24 +0800

    use PreferenceActivity and header instead

--- src/com/luzi82/snotebackup/MainActivity.java
+++ src/com/luzi82/snotebackup/MainActivity.java
@@ -1,59 +1,20 @@
 package com.luzi82.snotebackup;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import java.util.List;
+
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class MainActivity extends Activity implements MenuFragment.Callbacks {
-
-	// private boolean mTwoPane;
+public class MainActivity extends PreferenceActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		// MenuFragment menu_fragment = (MenuFragment)
-		// getFragmentManager().findFragmentById(R.id.menu_fragment);
-
-		// if (findViewById(R.id.item_detail_container) != null) {
-		// mTwoPane = true;
-		// ItemListFragment lhs = (ItemListFragment) getSupportFragmentManager()
-		// .findFragmentById(R.id.item_list);
-		// lhs.setActivateOnItemClick(true);
-		// }
 	}
 
-	@Override
-	public void onItemSelected(int id) {
-		T_T.v("id " + id);
-		// if (mTwoPane) {
-		Fragment fragment = null;
-		SNoteBackup.MenuItem item = SNoteBackup.MenuItem.values()[id];
-		switch (item) {
-		case HOME:
-			fragment = new HomeFragment();
-			break;
-		case SDCARD:
-			fragment = new SdcardSetttingFragment();
-			break;
-		case ABOUT:
-			fragment = new AboutFragment();
-			break;
-		default:
-			fragment = null;
-			break;
-		}
-		if (fragment != null) {
-			FragmentTransaction ft = getFragmentManager().beginTransaction();
-			ft.replace(R.id.item_detail_container, fragment);
-			ft.commit();
-		}
-		// } else {
-		// // Intent detailIntent = new Intent(this, ItemDetailActivity.class);
-		// // detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
-		// // startActivity(detailIntent);
-		// }
-	}
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.main, target);
+    }
+
 }
