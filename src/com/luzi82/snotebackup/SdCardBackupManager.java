26514c8029fe7f1644b10e3604d158ea1a5f7f68
luzi82 <luzi82@gmail.com>
2012-10-11 20:53:18 +0800

    file operation func

--- src/com/luzi82/snotebackup/SdCardBackupManager.java
+++ src/com/luzi82/snotebackup/SdCardBackupManager.java
@@ -1,30 +1,89 @@
 package com.luzi82.snotebackup;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SdCardBackupManager {
-	
-	private static final int BUFFER_SIZE = 512*1024;
+
+	private static final int BUFFER_SIZE = 512 * 1024;
 
 	public static void clone(File aFrom, File aTo) throws IOException {
-		if (!aFrom.exists()) {
+		if (!aFrom.exists())
 			throw new FileNotFoundException();
-		}
-		if (aTo.exists()) {
+		if (aTo.exists())
 			throw new IOException("File already exist");
-		}
 		if (aFrom.isDirectory()) {
 			aTo.mkdirs();
-			File[] list = aFrom.listFiles();
-			for (File from : list) {
-				File to = new File(aTo, from.getName());
-				clone(from, to);
+			cloneDir(aFrom, aTo);
+		} else if (aFrom.isFile()) {
+			byte[] buf = new byte[BUFFER_SIZE];
+			InputStream is = new FileInputStream(aFrom);
+			is = new BufferedInputStream(is);
+			OutputStream os = new FileOutputStream(aTo);
+			os = new BufferedOutputStream(os);
+			while (true) {
+				int len = is.read(buf);
+				if (len < 0)
+					break;
+				os.write(buf, 0, len);
 			}
-		}else if(aFrom.isFile()){
-			// TODO
+			is.close();
+			os.flush();
+			os.close();
 		}
+	}
+
+	public static void cloneDir(File aFrom, File aTo) throws IOException {
+		if (!aFrom.exists())
+			throw new FileNotFoundException();
+		if (!aTo.exists())
+			throw new FileNotFoundException();
+		if (!aFrom.isDirectory())
+			throw new IOException("Not directory");
+		if (!aTo.isDirectory())
+			throw new IOException("Not directory");
+		File[] list = aFrom.listFiles();
+		for (File from : list) {
+			File to = new File(aTo, from.getName());
+			clone(from, to);
+		}
+	}
+
+	public static boolean rm(File aFile) {
+		boolean t;
+		if (!aFile.exists())
+			return true;
+		if (aFile.isDirectory()) {
+			t = rmDir(aFile);
+			if (!t)
+				return false;
+		}
+		t = aFile.delete();
+		if (!t)
+			return false;
+		return true;
+	}
+
+	private static boolean rmDir(File aFile) {
+		boolean t;
+		if (!aFile.exists())
+			return false;
+		if (!aFile.isDirectory())
+			return false;
+		File[] list = aFile.listFiles();
+		for (File f : list) {
+			t = rm(f);
+			if (!t)
+				return false;
+		}
+		return true;
 	}
 
 }
