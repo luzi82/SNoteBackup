7b551ea88ac0d23a0a4d62bebb97f3f0c0e773e1
luzi82 <luzi82@gmail.com>
2012-10-13 16:11:58 +0800

    show backup notification

--- src/com/luzi82/snotebackup/SdcardBackupService.java
+++ src/com/luzi82/snotebackup/SdcardBackupService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -33,6 +34,20 @@ public class SdcardBackupService extends Service {
 			return START_STICKY;
 		}
 
+		// show notification
+		T_T.v("show notification");
+		Notification.Builder nb = new Notification.Builder(this);
+		// nb = nb.setTicker("ticker");
+		nb = nb.setContentTitle(getString(R.string.app_name));
+		nb = nb.setContentText(getString(R.string.notification_sdcard));
+		nb = nb.setOngoing(true);
+		nb = nb.setSmallIcon(R.drawable.ic_launcher);
+		// nb = nb.setLargeIcon(Bitmap.createBitmap(32, 32,
+		// Bitmap.Config.ARGB_8888));
+		Notification notification = nb.getNotification();
+		startForeground(R.string.app_name, notification);
+
+		// fire backup
 		long time = System.currentTimeMillis();
 		File sdcardDir = SNoteBackup.sdcardAppDir(this);
 		sdcardDir.mkdirs();
@@ -68,6 +83,7 @@ public class SdcardBackupService extends Service {
 	};
 
 	private synchronized void done() {
+		stopForeground(true);
 		stopSelf();
 		busy = false;
 	}
